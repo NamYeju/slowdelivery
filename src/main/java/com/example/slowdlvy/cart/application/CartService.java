52a7c04277e4eaa7b6ac81f870b76b06f884302f
jiyeon <sjy6736@naver.com>
2022-09-14 15:03:15 +0900

    FEAT: 장바구니 상품담기 기능구현

--- src/main/java/com/example/slowdlvy/cart/application/CartService.java
+++ src/main/java/com/example/slowdlvy/cart/application/CartService.java
@@ -18,52 +18,23 @@ public class CartService {
     private final CartRepository cartRepository;
     private final CartLineItemRepository cartLineItemRepository;
 
-    /**
-     * <장바구니 추가 과정>
-     * 프론트단에서 요청전에 이미 존재하는 cart안의 shopId와 새로 추가하려는 아이템의 shopId가 다를때 장바구니 교체여부를 물어보고 사용자가 교체를 원한다면 요청
-     * 1) 해당 member에 대한 cart가 존재하지 않는다면 새로운 카트 생성 후 아이템 추가
-     * 2) cart가 이미 존재한다면 그 cart의 shopId와 비교를 해주고 같으면 그냥 기존 cart에 아이템 추가해주기, 다르면 cart삭제 후 새로운 카트 생성 후 아이템 추가
-     */
     @Transactional
-    public Long add(CartRequestDto.addCart cartRequest){
-        Optional<Cart> cartOptional = cartRepository.findByMemberId(cartRequest.getMemberId());
+    public void addCartLineItem(Long memberId, CartRequestDto.addCartLineItem cartRequest){
+        Cart cart = getCartByMemberId(memberId);
         CartLineItem cartLineItem = cartRequest.toCartLineItem();
-
-        if(cartOptional.isPresent()){
-            Cart findCart = cartOptional.get();
-            if(findCart.isEqualByShop(cartRequest.getShopId())){
-                cartLineItem.setCart(findCart);
-                cartLineItemRepository.save(cartLineItem);
-                return findCart.getId();
-            }else{
-                cartRepository.delete(findCart);
-            }
-        }
-        Cart newCart = cartRequest.toCart();
-        cartLineItem.setCart(newCart);
-        cartRepository.save(newCart);
+        cartLineItem.setCart(cart);
         cartLineItemRepository.save(cartLineItem);
-        return newCart.getId();
-    }
-
-    /**
-     * <장바구니 삭제 과정>
-     * CartLineItem 삭제 후에 만약 Cart가 담긴 상품이 없이 빈 상태라면 Cart도 삭제해준다.
-     */
-    @Transactional
-    public void remove(Long cartLineItemId){
-        cartLineItemRepository.deleteById(cartLineItemId);
     }
 
     @Transactional
-    public void isEmptyRemove(Long cartId){
-        Optional<Cart> cartOptional = cartRepository.findById(cartId);
-        if(cartOptional.isPresent()){
-            if(cartOptional.get().isEmptyCart()){
-                cartRepository.deleteById(cartId);
-            }
+    public Cart getCartByMemberId(Long memberId){
+        Optional<Cart> cart = cartRepository.findByMemberId(memberId);
+        if(cart.isPresent()){
+            return cart.get();
+        }else{
+            Cart newCart = cartRepository.save(new Cart(memberId));
+            return newCart;
         }
     }
-
 
 }
--- src/main/java/com/example/slowdlvy/cart/presentation/CartController.java
+++ src/main/java/com/example/slowdlvy/cart/presentation/CartController.java
@@ -13,21 +13,8 @@ public class CartController {
     private final CartService cartService;
 
     @PostMapping("carts/{memberId}")
-    public ResponseEntity addCartItem(@PathVariable Long memberId, @RequestBody CartRequestDto.addCart cartRequest){
-        Long cartId = cartService.add(cartRequest);
-        return ResponseEntity.ok(cartId);
-        /**
-         * 리턴시 장바구니_INFO 나타내주는 URL로 연결해주도록 추후 수정해주기
-         */
-    }
-
-    @DeleteMapping("carts/{cartId}/{cartLineItemId}")
-    public ResponseEntity remove(@PathVariable Long cartId, @PathVariable Long cartLineItemId){
-        cartService.remove(cartLineItemId);
-        cartService.isEmptyRemove(cartId);
-        return ResponseEntity.ok("삭제 완료");
-        /**
-         * 리턴시 장바구니_INFO 나타내주는 URL로 연결해주도록 추후 수정해주기
-         */
+    public ResponseEntity addCartLineItem(@PathVariable Long memberId, @RequestBody CartRequestDto.addCartLineItem cartRequest){
+        cartService.addCartLineItem(memberId, cartRequest);
+        return ResponseEntity.ok("상품 담기 완료");
     }
 }
